6327e4be7b4b316b20baf782fdf6dfd4ad6f2773
Ian S <ian@iansapp.com>
2022-10-07 18:58:58 -0500

    Added Windows functionality Added the ability to run without args

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # csearch
 Wanted to refamiliarize myself with C++ so I decide to port my [Go application](https://github.com/IanSapp128/Census-Search) to it. It is a utility that pulls up the API documentation for the census table you provide it. Supported tables are **Detailed Tables**, **Subject Tables**, and **Data Profiles** for ACS 5 year survey data. I use this application almost daily for my job and figured I'd share it.
 
-I initially created this project as a Python script but startup time was pretty slow, so I wanted to try and make it with a compiled language. Currently works on Linux and Mac. I will add Windows functionality later when I am at a Windows machine and can test.
+I initially created this project as a Python script but startup time was pretty slow, so I wanted to try and make it with a compiled language. Currently works on Linux, Mac, and Windows.
 
-**Changes between versions**  
-The Go version has both the ability to pass the table name as an argument to get an instant browser window and a prompt for the user to enter a table name if nothing is passed. This is good if you want to create a shortcut and throw it on your taskbar like I do. Passing an arg to it is meant to be a fast way to search in the terminal when you have it open. I plan on adding the same functionality to this version when I have some more time (next update)
+Should now be feature complete with the original Go version. This will be the main project going forward if any changes are to be added. There is also now a [bash script](https://github.com/IanSapp128/csearch-bash) version for quick copy/pasting needs.
--- csearch.cpp
+++ csearch.cpp
@@ -4,9 +4,11 @@ using namespace std;
 void launchBrowser(string url)
 {
     #ifdef __linux__
-    string combUrl = "xdg-open " + url; // Use xdf if Linux
+    string combUrl = "xdg-open " + url; // Use xdf-open if Linux
     #elif __APPLE__ || __MACH__
     string combUrl = "open " + url; // Use open if Mac
+    #elif _WIN32
+    string combUrl = "start " + url; // start if Windows
     #endif
     const char *c = combUrl.c_str(); //Convert the passed string to a const char before passing into system command
     system(c);
@@ -15,9 +17,7 @@ void launchBrowser(string url)
 int main(int argc, char** argv)
 {
     #ifdef _WIN32
-    cout << "Windows is not currently supported\n";
-    return 0;
-    #elif __linux__
+    #elif __linux__ // Skip error check if the script runs on Mac or Linux
     #elif __APPLE__ || __MACH__
     #else
     cout << "Unknown system type. Not supported.\n";
@@ -25,24 +25,27 @@ int main(int argc, char** argv)
     #endif
 
     int argNums = argc;
-    if (argc > 1) {
-        string argVal = argv[1]; //Get the val for arg1
+    string argVal;
 
-        if (argVal[0] == 'B') {
-            string URL = "https://api.census.gov/data/2020/acs/acs5/groups/" + argVal + ".html";
-            launchBrowser(URL);
-        } else if (argVal[0] == 'S'){
-            string URL = "https://api.census.gov/data/2020/acs/acs5/subject/groups/" + argVal + ".html";
-            launchBrowser(URL);
-        } else if (argVal[0] == 'D'){
-            string URL = "https://api.census.gov/data/2020/acs/acs5/profile/groups/" + argVal + ".html";
-            launchBrowser(URL); 
-        } else{
-            cout<<"Not supported. Table name must begin with B, D, or S.\n";
-        }
+    if (argc > 1) {
+        argVal = argv[1]; //Get the val for arg1
 
     } else {
-        cout << "No argument passed\n";
+        cout << "Enter table name: "; //If no arg passed, prompt for table name
+        cin >> argVal;
+    }
+
+    if (argVal[0] == 'B') {
+        string URL = "https://api.census.gov/data/2020/acs/acs5/groups/" + argVal + ".html";
+        launchBrowser(URL);
+    } else if (argVal[0] == 'S'){
+        string URL = "https://api.census.gov/data/2020/acs/acs5/subject/groups/" + argVal + ".html";
+        launchBrowser(URL);
+    } else if (argVal[0] == 'D'){
+        string URL = "https://api.census.gov/data/2020/acs/acs5/profile/groups/" + argVal + ".html";
+        launchBrowser(URL); 
+    } else{
+        cout<<"Not supported. Table name must begin with B, D, or S.\n";
     }
 
   return 0;
